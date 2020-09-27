a8bb915e580fb4e0471da6b160e73879fc7d9bec
Vítor Lemos <31148456+vitorglemos@users.noreply.github.com>
2020-09-27 19:51:15 -0300

    credentials watson studio requirements

---
+++ credentials_watson_discovery.py
@@ -0,0 +1,6 @@
+wd_env_id = "WATSON_DISCOVERY_ENV_ID"
+wd_col_id = "WATSON_DISCOVERY_COLLECTION_TRAINED_ID"
+wd_ins_id = "WATSON_DISCOVERY_INSTANCE_ID"
+
+watson_discovery = f"https://api.au-syd.discovery.watson.cloud.ibm.com/instances/{wd_ins_id}/v1/environments/{wd_env_id}/collections/{wd_col_id}/query"
+apikey_discovery = "APIKEY_WATSON_DISCOVERY"
---
+++ credentials_watson_speech.py
@@ -0,0 +1,2 @@
+whatson_speech = "WATSON_CREDENTIALS_SPEECH_URL"
+apikey_speech = "APIKEY_WATSON_SPEECH"
